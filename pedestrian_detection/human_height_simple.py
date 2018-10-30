6b7bc57ceb7f38fb467869064e7c4bb2ed8f4ea8
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-10-30 15:47:08 +0900

    simple height estimate
    use focal length to estimate height

---
+++ pedestrian_detection/human_height_simple.py
@@ -0,0 +1,155 @@
+# Code adapted from Tensorflow Object Detection Framework
+# https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
+# Tensorflow Object Detection Detector
+
+import numpy as np
+import tensorflow as tf
+import cv2
+import time
+import pyrealsense2 as rs
+
+class DetectorAPI:
+    def __init__(self, path_to_ckpt):
+        self.path_to_ckpt = path_to_ckpt
+
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        self.default_graph = self.detection_graph.as_default()
+        self.sess = tf.Session(graph=self.detection_graph)
+
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+    def processFrame(self, image):
+        # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image, axis=0)
+        # Actual detection.
+        start_time = time.time()
+        (boxes, scores, classes, num) = self.sess.run(
+            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_np_expanded})
+        end_time = time.time()
+
+        #print("Elapsed Time:", end_time-start_time)
+
+        im_height, im_width,_ = image.shape
+        boxes_list = [None for i in range(boxes.shape[1])]
+        for i in range(boxes.shape[1]):
+            boxes_list[i] = (int(boxes[0,i,0] * im_height),
+                        int(boxes[0,i,1]*im_width),
+                        int(boxes[0,i,2] * im_height),
+                        int(boxes[0,i,3]*im_width))
+
+        return boxes_list, scores[0].tolist(), [int(x) for x in classes[0].tolist()], int(num[0])
+
+    def close(self):
+        self.sess.close()
+        self.default_graph.close()
+
+if __name__ == "__main__":
+    model_path = 'D:/Workspace/PedestrianDetection/pedestrian_detection/frozen_inference_graph.pb'
+    odapi = DetectorAPI(path_to_ckpt=model_path)
+    threshold = 0.7
+
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    # Start streaming
+
+    profile = pipeline.start(config)
+
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    print("Depth Scale is: ", depth_scale)
+
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 1  # 1 meter
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    while True:
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        # Intrinsics & Extrinsics
+        depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
+        color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+        depth_to_color_extrin = depth_frame.profile.get_extrinsics_to(
+            color_frame.profile)
+
+        print("focal length %s an d%s ", depth_intrin.fx, depth_intrin.fy)
+        #####################################################################
+
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
+
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        ######################################################################
+
+        img = color_image
+
+        boxes, scores, classes, num = odapi.processFrame(img)
+
+        # Visualization of the results of a detection.
+
+        for i in range(len(boxes)):
+            # Class 1 represents human
+            if classes[i] == 1 and scores[i] > threshold:
+                box = boxes[i]
+                cv2.rectangle(img,(box[1],box[0]),(box[3],box[2]),(255,0,0),2)
+
+                # get distance of person from the camera, from the middle pixel
+                midpoint_x = np.int(box[1] + ((box[3] - box[1]) / 2))
+                midpoint_y = np.int(box[0] + ((box[2] - box[0]) / 2))
+
+####################################################################################################################
+
+
+                depth_mid =aligned_depth_frame.get_distance(midpoint_x, midpoint_y)
+                print("human distance from camera : ", depth_mid)
+
+                height_estimate = (depth_mid * (box[2] - box[0]))/depth_intrin.fx
+                print("human height estimate : ", height_estimate)
+                height_str = str(height_estimate) + " m"
+####################################################################################################################
+
+                cv2.putText(img, height_str, (box[1], box[0]+35), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+
+        cv2.imshow("preview", img)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            break
