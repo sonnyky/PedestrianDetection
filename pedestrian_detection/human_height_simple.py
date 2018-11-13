2152ddd8ea178fc5b472ba7993d7a39a225be908
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-11-14 01:24:58 +0900

    added multitracker

--- pedestrian_detection/human_height_simple.py
+++ pedestrian_detection/human_height_simple.py
@@ -1,4 +1,4 @@
-# Code adapted from Tensorflow Object Detection Framework
+    # Code adapted from Tensorflow Object Detection Framework
 # https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
 # Tensorflow Object Detection Detector
 
@@ -7,6 +7,36 @@ import tensorflow as tf
 import cv2
 import time
 import pyrealsense2 as rs
+
+trackerTypes = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+
+
+def createTrackerByName(trackerType):
+    # Create a tracker based on tracker name
+    if trackerType == trackerTypes[0]:
+        tracker = cv2.TrackerBoosting_create()
+    elif trackerType == trackerTypes[1]:
+        tracker = cv2.TrackerMIL_create()
+    elif trackerType == trackerTypes[2]:
+        tracker = cv2.TrackerKCF_create()
+    elif trackerType == trackerTypes[3]:
+        tracker = cv2.TrackerTLD_create()
+    elif trackerType == trackerTypes[4]:
+        tracker = cv2.TrackerMedianFlow_create()
+    elif trackerType == trackerTypes[5]:
+        tracker = cv2.TrackerGOTURN_create()
+    elif trackerType == trackerTypes[6]:
+        tracker = cv2.TrackerMOSSE_create()
+    elif trackerType == trackerTypes[7]:
+        tracker = cv2.TrackerCSRT_create()
+    else:
+        tracker = None
+        print('Incorrect tracker name')
+        print('Available trackers are:')
+        for t in trackerTypes:
+            print(t)
+
+    return tracker
 
 class DetectorAPI:
     def __init__(self, path_to_ckpt):
@@ -62,9 +92,15 @@ class DetectorAPI:
 if __name__ == "__main__":
     model_path = 'D:/Workspace/PedestrianDetection/pedestrian_detection/frozen_inference_graph.pb'
     odapi = DetectorAPI(path_to_ckpt=model_path)
-    threshold = 0.7
+    threshold = 0.9
 
-    # Configure depth and color streams
+    trackerType = "CSRT"
+
+    ## Select boxes
+    bboxes = []
+    colors = []
+
+        # Configure depth and color streams
     pipeline = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
@@ -90,6 +126,21 @@ if __name__ == "__main__":
     align_to = rs.stream.color
     align = rs.align(align_to)
 
+    # Create MultiTracker object
+    multiTracker = cv2.MultiTracker_create()
+
+    # Wait for a coherent pair of frames: depth and color
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    # Intrinsics & Extrinsics
+    depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
+    color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+    depth_to_color_extrin = depth_frame.profile.get_extrinsics_to(
+        color_frame.profile)
+
+    print("focal length %s an d%s ", depth_intrin.fx, depth_intrin.fy)
+
     while True:
 
         # Wait for a coherent pair of frames: depth and color
@@ -99,13 +150,7 @@ if __name__ == "__main__":
         if not depth_frame or not color_frame:
             continue
 
-        # Intrinsics & Extrinsics
-        depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
-        color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
-        depth_to_color_extrin = depth_frame.profile.get_extrinsics_to(
-            color_frame.profile)
 
-        print("focal length %s an d%s ", depth_intrin.fx, depth_intrin.fy)
         #####################################################################
 
         # Align the depth frame to color frame
@@ -125,12 +170,20 @@ if __name__ == "__main__":
         boxes, scores, classes, num = odapi.processFrame(img)
 
         # Visualization of the results of a detection.
-
+        num_of_people = 0
         for i in range(len(boxes)):
             # Class 1 represents human
             if classes[i] == 1 and scores[i] > threshold:
+                num_of_people += 1
                 box = boxes[i]
                 cv2.rectangle(img,(box[1],box[0]),(box[3],box[2]),(255,0,0),2)
+                human_roi = img[box[0]:box[2], box[1]:box[3]]
+                if len(bboxes) < num_of_people:
+                    print("there are untracked people")
+                    bboxes.append(human_roi)
+                    #print("current bboxes length : ", len(bboxes))
+                    #colors.append((randint(64, 255), randint(64, 255), randint(64, 255)))
+                    #multiTracker.add(createTrackerByName(trackerType), img, human_roi)
 
                 # get distance of person from the camera, from the middle pixel
                 midpoint_x = np.int(box[1] + ((box[3] - box[1]) / 2))
@@ -140,14 +193,14 @@ if __name__ == "__main__":
 
 
                 depth_mid =aligned_depth_frame.get_distance(midpoint_x, midpoint_y)
-                print("human distance from camera : ", depth_mid)
+                #print("human distance from camera : ", depth_mid)
 
                 height_estimate = (depth_mid * (box[2] - box[0]))/depth_intrin.fx
-                print("human height estimate : ", height_estimate)
+                #print("human height estimate : ", height_estimate)
                 height_str = str(height_estimate) + " m"
+                cv2.putText(img, height_str, (box[1], box[0] + 35), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
 ####################################################################################################################
-
-                cv2.putText(img, height_str, (box[1], box[0]+35), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+        #print("Number of people detected : ", num_of_people)
 
         cv2.imshow("preview", img)
         key = cv2.waitKey(1)
